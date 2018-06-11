4a4c7746d098dec55d347f6d70140fb3a35bc52e
Bernat Gómez <bernat.gomez@gmail.com>
2018-06-11 19:42:17 +0200

    Modify splash screen

--- js/presenter_splash.js
+++ js/presenter_splash.js
@@ -2,6 +2,12 @@
 // Functions used in splash.html
 //////////////////////////////////////////////////////////////
 
+var ID_TITLE = "mainTitle";
+
+function setTexts() {
+	window.document.getElementById(ID_TITLE).innerHTML = LANG_JSON_DATA["app_name"];
+}
+
 function initComponents() {
 	if (storageHasNoKeys()) {
 		initStorage();
