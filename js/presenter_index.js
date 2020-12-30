2b6f82719d2adc77f060c51a8c3a516c3615c566
Bernat Gómez <bernat.gomez@gmail.com>
2020-12-30 11:11:19 +0100

    Add stats

--- js/presenter_index.js
+++ js/presenter_index.js
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////////////////////
-// Functions used in splash.html
+// Functions used in index.html
 //////////////////////////////////////////////////////////////
 
 var ID_TITLE = "mainTitle";
@@ -9,6 +9,7 @@ var ID_ITEM_2 = "item2";
 var ID_ITEM_3 = "item3";
 var ID_ITEM_4 = "item4";
 var ID_ITEM_5 = "item5";
+var ID_ITEM_6 = "item6";
 
 function setTexts() {
 	window.document.getElementById(ID_TITLE).innerHTML = LANG_JSON_DATA["app_name"];
@@ -17,5 +18,6 @@ function setTexts() {
 	window.document.getElementById(ID_ITEM_2).innerHTML = LANG_JSON_DATA["option_finish"];
 	window.document.getElementById(ID_ITEM_3).innerHTML = LANG_JSON_DATA["option_discard"];
 	window.document.getElementById(ID_ITEM_4).innerHTML = LANG_JSON_DATA["option_history"];
-	window.document.getElementById(ID_ITEM_5).innerHTML = LANG_JSON_DATA["option_tutorial"];
+	window.document.getElementById(ID_ITEM_5).innerHTML = LANG_JSON_DATA["option_stats"];
+	window.document.getElementById(ID_ITEM_6).innerHTML = LANG_JSON_DATA["option_tutorial"];
 }
---
+++ js/presenter_stats.js
@@ -0,0 +1,43 @@
+//////////////////////////////////////////////////////////////
+// Functions used in stats.html
+//////////////////////////////////////////////////////////////
+
+
+
+
+var ID_TITLE = "mainTitle";
+var ID_SUBTITLE = "secondaryTitle";
+
+function setTexts() {
+	window.document.getElementById(ID_TITLE).innerHTML = LANG_JSON_DATA["app_name"];
+	window.document.getElementById(ID_SUBTITLE).innerHTML = LANG_JSON_DATA["app_name"];
+}
+
+function initGraph() {
+	var page = document.getElementById("stats"),
+		graphEl = document.getElementById("graph"),
+		titleMarquee = page.querySelector("header"),
+		graphWidget,
+		marquee;
+
+	page.addEventListener("pagebeforeshow", function () {
+		graphWidget = tau.widget.Graph(graphEl);
+	});
+	
+	page.addEventListener("pageshow", function () {
+		marquee = tau.widget.Marquee(titleMarquee, {
+			iteration: "infinite",
+			marqueeStyle: "scroll",
+			ellipsisEffect: "none",
+			speed: 40,
+			delay: 1000
+		});
+	});
+
+	page.addEventListener("pagebeforehide", function () {
+		graphWidget.destroy();
+		marquee.destroy();
+	});
+	
+	console.log("Done!");
+}
