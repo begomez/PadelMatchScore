f2b9d7278721f4eaafdb25a125cbc2b928368991
Bernat Gómez <bernat.gomez@gmail.com>
2020-12-30 11:49:34 +0100

    Modify tutorial

--- js/presenter_tutorial.js
+++ js/presenter_tutorial.js
@@ -3,8 +3,9 @@
 //////////////////////////////////////////////////////////////
 
 var IMG_STEP_1 = "tut_step_1.png";
-var IMG_STEP_2 = "tut_step_3.png";
-var IMG_STEP_3 = "tut_step_4.png";
+var IMG_STEP_2 = "tut_step_2.png";
+var IMG_STEP_3 = "tut_step_3.png";
+var IMG_STEP_4 = "tut_step_4.png";
 
 var ID_IMG = "imgTutorial";
 var ID_MSG = "msgTutorial";
@@ -14,6 +15,7 @@ var ID_BTN_SKIP = "btnSkip";
 var FIRST_PAGE = 1;
 var SECOND_PAGE = 2;
 var THIRD_PAGE = 3;
+var FOURTH_PAGE = 4;
 
 var page = 1;
 
@@ -38,8 +40,12 @@ function configPage(page) {
 			break;
 		case THIRD_PAGE:
 			window.document.getElementById(ID_MSG).innerHTML = LANG_JSON_DATA["tutorial_step3"];
-			window.document.getElementById(ID_BTN).innerHTML = LANG_JSON_DATA["action_done"];
 			loadImage(ID_IMG, "../images/"+ IMG_STEP_3);
+			break;
+		case FOURTH_PAGE:
+			window.document.getElementById(ID_MSG).innerHTML = LANG_JSON_DATA["tutorial_step4"];
+			window.document.getElementById(ID_BTN).innerHTML = LANG_JSON_DATA["action_done"];
+			loadImage(ID_IMG, "../images/"+ IMG_STEP_4);
 			break;
 		default:
 			onSkipClick();
